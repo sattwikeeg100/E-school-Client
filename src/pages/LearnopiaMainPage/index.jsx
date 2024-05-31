45652648284a4f40eea592c34973d97c83b6f165
Sattwikee Ghosh <sattwikeeghosh95@gmail.com>
2024-05-31 10:53:01 +0530

    more fixes

--- src/pages/LearnopiaMainPage/index.jsx
+++ src/pages/LearnopiaMainPage/index.jsx
@@ -164,42 +164,8 @@ export default function LeranopiaMainPage() {
             <div className="flex flex-col items-center justify-start w-full gap-10">
               <div className="flex flex-col items-start justify-start w-full pt-0.5 gap-2.5">
                 <h1 className="text-4xl font-bold sm:text-3xl sm:p-4 mb-4">
-                  Explore Our Other Popular Courses
+                  Explore Our Popular Books!
                 </h1>
-                <div className="flex flex-row justify-start w-[75%] sm:w-full gap-[42px] sm:flex-col sm:mx-4 sm:gap-[8px]">
-                  <form
-                    className="flex w-[77%] md:w-full"
-                    onSubmit={(e) => e.preventDefault()}
-                  >
-                    <input
-                      type="text"
-                      placeholder="Search by Book Name"
-                      value={searchBarValue}
-                      onChange={handleSearch}
-                      className="px-4 w-[80%] bg-white font-medium rounded-l-md border-orange-200 focus:ring-orange-300 sm:text-sm"
-                    />
-                    <button
-                      type="submit"
-                      className="w-[20%] md:w-[25%] bg-orange-200 font-medium text-white rounded-r-md hover:bg-orange-300 focus:outline-none sm:text-sm"
-                    >
-                      Search
-                    </button>
-                  </form>
-
-                  <SelectBox
-                    indicator={
-                      <Img
-                        src="images/img_arrowdown_red_300_01.svg"
-                        alt="arrow_down"
-                      />
-                    }
-                    name="sortbylatest"
-                    placeholder="Sort by: Latest"
-                    options={dropDownOptions}
-                    onChange={handleSort}
-                    className="w-[33%] gap-px font-medium md:w-full sm:h-8 sm:text-sm"
-                  />
-                </div>
               </div>
               <div className="justify-center w-full gap-[15px] grid-cols-3 md:grid-cols-1 grid min-h-[auto]">
                 {loading
